d69608d27c591fb043416adf24172bbf1c88bb0c
adamteddychang <adamchang0725@gmail.com>
2022-04-05 08:42:39 +0200

    added controllers for api

---
+++ app/controllers/api/vi/houses_controller.rb
@@ -0,0 +1,11 @@
+class Api::Vi::HousesController < ApplicationController
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+end
---
+++ app/controllers/api/vi/users_controller.rb
@@ -0,0 +1,3 @@
+class Api::Vi::UsersController < ApplicationController
+  def index; end
+end
