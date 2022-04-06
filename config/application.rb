56852ca2cfdbc6631f5273c3895f2592ec802cc5
adamteddychang <adamchang0725@gmail.com>
2022-04-06 17:13:47 +0200

    edited cors

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module VacayhomeBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: %i[get post options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
 
