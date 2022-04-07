05edde1c605d97a8a8f0b790aedf23412ae1b68c
adamteddychang <adamchang0725@gmail.com>
2022-04-07 12:37:49 +0200

    fix spelling

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :houses, only: [:index, :show,:new, :create, :update, :destroy] 
       resources :bookings, only: [:index, :show,:new, :create, :update, :destroy]
-      resources :users, only:[:indes, :create, :show]
+      resources :users, only:[:index, :create, :show]
     end
   end
 end
