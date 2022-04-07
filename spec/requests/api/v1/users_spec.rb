44fb4a29442af410512a0b9ced11ec7c51eccf39
adamteddychang <adamchang0725@gmail.com>
2022-04-07 11:49:56 +0200

    added user index page

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,6 +2,20 @@ require 'swagger_helper'
 
 RSpec.describe 'api/v1/users', type: :request do
   path '/api/v1/users' do
+
+    get('list all users') do
+      tags 'Users'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
     post('create user') do
       tags 'Users'
       consumes 'application/json'
